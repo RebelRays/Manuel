c954349eb5fa7ad69549941c82de47ca78e8a2f1
unknown <someuseremail@someuserexample.com>
2019-07-31 21:27:40 +0200

    dirs

--- Camera/__init__.py
+++ Camera/__init__.py
@@ -2,14 +2,17 @@ from picamera import PiCamera
 from time import sleep
 from datetime import datetime
 
-pi_camera = PiCamera()
+
 class CameraHandler:
     todo=1
 
     def takePicture(self):
-        pi_camera.resolution = (64, 64)
-        now_Str = datetime.today().strftime('%Y%m%d-%H%M%S')
-        pi_camera.capture('./DataRecording/Images/%s.png' % now_Str)
-        pi_camera.close()
+        pi_camera = PiCamera()
+        try:
+            #pi_camera.resolution = (256, )
+            now_Str = datetime.today().strftime('%Y%m%d-%H%M%S')
+            pi_camera.capture('./DataRecording/Images/%s.png' % now_Str)
+        finally:
+            pi_camera.close()
     
     
