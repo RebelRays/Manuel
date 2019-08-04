f965e045c0968f3a9a60fe2b4abbb704cf934781
unknown <someuseremail@someuserexample.com>
2019-08-05 00:19:14 +0200

    Minion update

--- Minion.py
+++ Minion.py
@@ -78,7 +78,114 @@ except NameError:
 ardy = ArduinoCommunication.ArduinoCommunication()
 camera = Camera.CameraHandler()
 Wifi = WifiSignals.WifiSignals()
-LoopNo = 20
+
+RecordInverseMoves = []
+def ExecCommand(user_command):
+
+
+    ########################
+    # Enviroment Commands
+    if(user_command.upper() == 'Q'):
+        LoopNo=0
+        break
+    elif(user_command.upper() == 'W'):
+        time.sleep(0.5)
+        RecordInverseMoves.append('W')
+    elif(user_command.upper() == 'GOBACK'):
+        for command in RecordInverseMoves:
+            ExecCommand(command)
+        ExecCommand("S")
+    ########################
+    # Engine Commands
+    if(user_command.upper() == 'F'):
+        engines.RobotMoveForward()
+        RecordInverseMoves.append('B')
+    elif(user_command.upper() == 'B'):
+        engines.RobotMoveBack()
+        RecordInverseMoves.append('F')
+    elif(user_command.upper() == 'L'):
+        engines.RobotMoveLeft()
+        RecordInverseMoves.append('R')
+        #engines.RobotMoveRightWheels()
+    elif(user_command.upper() == 'R'):
+        engines.RobotMoveRight()
+        RecordInverseMoves.append('L')
+    elif(user_command.upper() == 'S'):
+        print("Stopping")
+        engines.RobotStop()
+        RecordInverseMoves.append('S')
+    #########################
+    #SENSORS
+    elif(user_command.upper() == 'U'):
+        print("Ultrasound Distance = " +str(usDistance.UltraDistance))
+    elif(user_command.upper() == 'PIC'):
+        camera.takePicture()
+    elif(user_command.upper() == 'WIFI'):
+        Wifi.Record()
+    #########################
+    #ROBOT CLAWS
+    elif(user_command.upper() == 'G'):
+        ardy.printServoLocations()
+    elif(user_command.upper() == "READ"):
+        ardy.printAll()
+    elif(user_command.upper() == 'P'):
+        ardy.pingArdy()
+    elif(user_command[0:3].upper() == 'SET'):
+        ardy.MoveServo(user_command[3], int(user_command[4:]))
+    elif(user_command.upper() == 'UPP'):
+        ardy.MoveServo('4', 175)
+        time.sleep(0.8)
+        ardy.MoveServo('1', 90)
+        time.sleep(0.8)
+        ardy.MoveServo('3', 70)
+        time.sleep(0.8)
+        ardy.MoveServo('2', 177)
+        time.sleep(0.8)
+    elif(user_command.upper() == 'DOWN'):
+        ardy.MoveServo('4', 95)
+        time.sleep(0.8)
+        ardy.MoveServo('1', 90)
+        time.sleep(0.8)
+        ardy.MoveServo('2', 5)
+        time.sleep(0.8)
+        ardy.MoveServo('3', 40)
+        time.sleep(0.8)
+    elif(user_command.upper() == 'HOLD'):
+        ardy.MoveServo('4', 175)
+        time.sleep(0.8)
+        ardy.MoveServo('1', 90)
+        time.sleep(0.8)
+        ardy.MoveServo('2', 20)
+        time.sleep(0.8)
+        ardy.MoveServo('3', 110)
+        time.sleep(0.8)
+    #########################
+    #AI
+    elif(user_command.upper() == 'HUNT'):
+        NO_OF_SEARCHTURNS_ALLOWED = 5
+        while(NO_OF_SEARCHTURNS_ALLOWED > 0):
+            imagename = camera.takePicture()
+            res = HunterAI.Descision(imagename)
+            if(res == "grab"):
+                print("grabbing")
+                ardy.MoveServo('4', 170)
+                break
+            elif (res == "left"):
+                ExecCommand("L")
+                ExecCommand("W")
+                ExecCommand("S")
+            elif (res == "forward"):
+                ExecCommand("F")
+                ExecCommand("W")
+                ExecCommand("S")
+            elif (res == "Did not find anything"):
+                print("left searching")
+                ExecCommand("L")
+                ExecCommand("W")
+                ExecCommand("S")
+                NO_OF_SEARCHTURNS_ALLOWED = NO_OF_SEARCHTURNS_ALLOWED - 1
+
+LoopNo = 40
 try:
     while(LoopNo>0):
         print(LoopNo)
@@ -89,116 +196,8 @@ try:
 
         for user_command in user_commands:
             print("user_command : " + user_command)
-            if(user_command.upper() == 'F'):
-                engines.RobotMoveForward()
-            elif(user_command.upper() == 'B'):
-                engines.RobotMoveBack()
-            elif(user_command.upper() == 'L'):
-                engines.RobotMoveLeft()
-                #engines.RobotMoveRightWheels()
-            elif(user_command.upper() == 'R'):
-                engines.RobotMoveRight()
-            elif(user_command.upper() == 'S'):
-                print("Stopping")
-                engines.RobotStop()
-            elif(user_command.upper() == 'G'):
-                ardy.printServoLocations()
-            elif(user_command.upper() == 'U'):
-               print("Ultrasound Distance = " +str(usDistance.UltraDistance))
-            elif(user_command.upper() == 'SIDE'):
-                #ardy.MoveServo('1', 10)
-                #ardy.MoveServo('2', 10)
-                #ardy.MoveServo('3', 80)
-                #ardy.MoveServo('4', 160)
-                ardy.MoveServo('1', 5)
-
-                ardy.MoveServo('2', 5)
-                ardy.MoveServo('3', 70)
-                ardy.MoveServo('4', 160)
-            elif(user_command.upper() == 'START'):
-                #ardy.MoveServo('1', 87)
-                #ardy.MoveServo('2', 160)
-                #ardy.MoveServo('3', 60)
-                #ardy.MoveServo('4', 166)
-                ardy.MoveServo('4', 175)
-                time.sleep(0.8)
-                ardy.MoveServo('1', 90)
-                time.sleep(0.8)
-                ardy.MoveServo('3', 70)
-                time.sleep(0.8)
-                ardy.MoveServo('2', 177)
-                time.sleep(0.8)
-            elif(user_command.upper() == 'DOWN'):
-                ardy.MoveServo('4', 100)
-                time.sleep(0.8)
-                ardy.MoveServo('1', 90)
-                time.sleep(0.8)
-                ardy.MoveServo('2', 5)
-                time.sleep(0.8)
-                ardy.MoveServo('3', 40)
-                time.sleep(0.8)
-            elif(user_command.upper() == 'HUNT'):
-                NO_OF_SEARCHTURNS_ALLOWED = 5
-                while(NO_OF_SEARCHTURNS_ALLOWED > 0):
-                    imagename = camera.takePicture()
-                    res = HunterAI.Descision(imagename)
-                    if(res == "grab"):
-                        print("grabbing")
-                        ardy.MoveServo('4', 170)
-                        break
-                    elif (res == "left"):
-                        print("Moving left")
-                        engines.RobotMoveLeft()
-                        time.sleep(0.5)
-                        engines.RobotStop()
-                    elif (res == "forward"):
-                        print("Forward")
-                        engines.RobotMoveForward()
-                        time.sleep(0.5)
-                        engines.RobotStop()
-                    elif (res == "Did not find anything"):
-                        print("left searching")
-                        engines.RobotMoveLeft()
-                        time.sleep(1)
-                        engines.RobotStop()
-                        NO_OF_SEARCHTURNS_ALLOWED = NO_OF_SEARCHTURNS_ALLOWED - 1
-                    
-            elif(user_command.upper() == 'PIC'):
-                camera.takePicture()
-            elif(user_command.upper() == 'WIFI'):
-                Wifi.Record()
-            elif(user_command.upper() == "READ"):
-                #print("Checking Serial reply")
-                ardy.printAll()
-            elif(user_command[0:3].upper() == 'SET'):
-                ardy.MoveServo(user_command[3], int(user_command[4:]))
-            elif(user_command.upper() == 'P'):
-                ardy.pingArdy()
-            elif(user_command.upper() == 'H'):
-                ardy.printLastCommand()
-            
-            elif(user_command.upper() == 'Q'):
-                LoopNo=0
-                break
-            elif(user_command.upper() == 'W'):
-                time.sleep(0.5)
-            
-            LoopNo=LoopNo-1
-        #UltraSoundHandler
-        #distance = UltraSound.getUltaSoundDistance()
-        
-        #print("Distance to Base the decsions On = " +str(usDistance.UltraDistance))
-        #if(usDistance.UltraDistance > 0.25):
-        #    print("Forward")
-        #    engines.RobotMoveForward()
-            #engines.RobotMoveBack()
-            #engines.RobotMoveLeft()
-        #else:
-        #    engines.RobotStop()
-
-        #print(distance)
-        #time.sleep(0.2)
-        #LoopNo=LoopNo-1
+            ExecCommand(user_commands)
+        LoopNo=LoopNo-1
 finally:
     ardy.cleanup()  
     gpio.cleanup() # this ensures a clean exit
