78a511c1309c12dc2e1aa771986bf6a9b4abad27
unknown <someuseremail@someuserexample.com>
2019-08-14 00:01:04 +0200

    Cleanup

--- ttensor.py
+++ ttensor.py
@@ -2,7 +2,31 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 import tensorflow as tf
 import numpy as np
+from tensorflow import keras
 
-tf.logging.set_verbosity(tf.logging.INFO)
+tf.logging.set_verbosity(tf.compat.v1.logging.INFO)
+
+
+# Returns a short sequential model
+def create_model():
+  model = tf.keras.models.Sequential([
+    keras.layers.Dense(512, activation=tf.keras.activations.relu, input_shape=(784,)),
+    keras.layers.Dropout(0.2),
+    keras.layers.Dense(10, activation=tf.keras.activations.softmax)
+  ])
+
+  model.compile(optimizer=tf.keras.optimizers.Adam(),
+                loss=tf.keras.losses.sparse_categorical_crossentropy,
+                metrics=['accuracy'])
+
+  return model
+
+
+# Create a basic model instance
+model = create_model()
+model.summary()
+
+
 
 print("Done")
+
