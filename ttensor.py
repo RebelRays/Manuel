b59c47c395d9b5eea1df4d7d138925d7ac9ee6da
unknown <someuseremail@someuserexample.com>
2019-08-14 00:04:10 +0200

    Fix randome

--- ttensor.py
+++ ttensor.py
@@ -18,13 +18,27 @@ def create_model():
   model.compile(optimizer=tf.keras.optimizers.Adam(),
                 loss=tf.keras.losses.sparse_categorical_crossentropy,
                 metrics=['accuracy'])
+  
 
   return model
 
 
+model2 = tf.keras.models.Sequential([
+  tf.keras.layers.Conv2D(60, (3, 3), activation='relu', input_shape=(120, 120,3)),
+  tf.keras.layers.MaxPooling2D((2, 2)),
+  tf.keras.layers.Conv2D(15, (3, 3), activation='relu'),
+  tf.keras.layers.MaxPooling2D((2, 2)),
+  #tf.keras.layers.Conv2D(12, (3, 3), activation='relu'),
+  tf.keras.layers.Flatten(),
+  tf.keras.layers.Dense(8, activation='relu'),
+  tf.keras.layers.Dropout(0.4),
+  #tf.keras.layers.Dense(2, activation='sigmoid')
+  tf.keras.layers.Dense(2, activation='softmax')
+])
+
 # Create a basic model instance
-model = create_model()
-model.summary()
+#model = create_model()
+#model.summary()
 
 
 
