57fff5fc76e73de288d5b10947984b4b3f36add2
unknown <someuseremail@someuserexample.com>
2019-08-18 12:16:55 +0200

    Tensor

--- AITensorflow/__init__.py
+++ AITensorflow/__init__.py
@@ -22,7 +22,7 @@ def load_model():
     tf.keras.layers.Dense(2, activation='softmax')
     ])
 
-    modelfile= "tensormodel/cp-0015.ckpt"
+    modelfile= "tensormodel/cp-0085.ckpt"
     model2.load_weights(modelfile)
     model = model2
 
