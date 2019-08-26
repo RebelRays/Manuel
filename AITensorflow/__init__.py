efe4f6e01df2114c10620efde1dbe72ed3ddbe3d
unknown <someuseremail@someuserexample.com>
2019-08-27 00:36:13 +0200

    model

--- AITensorflow/__init__.py
+++ AITensorflow/__init__.py
@@ -54,7 +54,24 @@ def load_model4():
     global model
     global modelInputDim
 
-    model2 =tf.keras.models.load_model("tensormodel/best")
+    #model2 =tf.keras.models.load_model("tensormodel/best")
+    model = tf.keras.models.Sequential([
+    tf.keras.layers.Conv2D(42, (3, 3), activation='relu', input_shape=(60,60, 3)),
+    tf.keras.layers.MaxPooling2D((2, 2)),
+    tf.keras.layers.Dropout(0.04),
+    tf.keras.layers.Conv2D(24, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D((2, 2)),
+    #tf.keras.layers.Conv2D(12, (3, 3), activation='relu'),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(8, activation='relu'),
+    tf.keras.layers.Dropout(0.3),
+    #tf.keras.layers.Dense(2, activation='sigmoid')
+    tf.keras.layers.Dense(2, activation='softmax')
+    ])
+    
+    
+    modelfile= "tensormodel/cp_60-60_40-d-20-003_0030.ckpt"
+    model2.load_weights(modelfile)
     model = model2
     modelInputDim = (60,60)
 
